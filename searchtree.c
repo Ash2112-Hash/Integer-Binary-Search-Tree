5736d978f61f1f51a2459aee3919edc0f729947a
Ashwin <unnithanash@gmail.com>
2022-12-23 12:09:13 -0500

    first commit

--- searchtree.c
+++ searchtree.c
@@ -1,13 +1,15 @@
+/*
+Imports the corresponding libraries in c
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
 
 /*
-Defines the functions for 
-
-
+Defines the functions for comprising the node structure
+    1. node_struct creates the fundamental structure of the node using struct
+    2. new_node_struct allocates memory in creating a new node
 */
-
 
 // defines the the structure to create the node and refers to it as NODE
 typedef struct node_struct {
@@ -26,17 +28,33 @@ NODE* new_node(int value) {
 
 
 
+/*
+Defines the functions for ordering and filtering the binary tree
+    1. insert1 places a new node in the tree (on the left or right subtree) based on key value recursively
+    2. insert is a general function to insert a node within the tree (by recursively calling insert1) or creates a new node
+    3. PreOrder, InOrder and PostOrder functions display nodes of the tree in corresponding order by calling them recursively
+*/
+
 void insert1(NODE* p,int value) {
-  if (value < p->value) {
-    if (p->left == 0)
-      p->left = new_node(value);
-    else
-      insert1(p->left,value);
-  }else if (value > p->value) {
-    if (p->right == 0) 
+  if (value < p->value) {    
+    if (p->left == 0){
+        p->left = new_node(value);
+    }
+      
+    else{
+        insert1(p->left,value);
+    }
+      
+  }
+  
+  else if (value > p->value) {
+    if (p->right == 0){ 
       p->right = new_node(value);
-    else
+    }
+
+    else{
       insert1(p->right,value);
+    }
   }
 }
   
@@ -46,6 +64,7 @@ NODE* insert(NODE* root,int value) {
     root = new_node(value);
     return root;
   }
+
   insert1(root,value);
   return root;
 }
@@ -53,8 +72,14 @@ NODE* insert(NODE* root,int value) {
 
 void PreOrder1(NODE* p) {
   printf("%d ",p->value);
-  if (p->left) PreOrder1(p->left);
-  if (p->right) PreOrder1(p->right);
+
+  if (p->left) {
+    PreOrder1(p->left);
+  }
+
+  if (p->right){ 
+  PreOrder1(p->right);
+  }
 }
 
 void PreOrder(NODE* root) {
@@ -62,9 +87,11 @@ void PreOrder(NODE* root) {
     printf("empty tree\n");
     return;
   }
+
   PreOrder1(root);
   printf("\n");
 }
+
 
 void InOrder1(NODE* p) {
   if (p->left) InOrder1(p->left);
@@ -96,6 +123,11 @@ void PostOrder(NODE* root) {
   printf("\n");
 }
 
+
+/*
+main function to handle adding user-inputed numbers to the tree and establishing the fundamental structure
+*/
+
 int main() {
   int n;
   char buf[500];
@@ -104,15 +136,21 @@ int main() {
   while(1) {
     printf("enter positive integer or 'quit': ");
     scanf("%s",buf);
-    if (strcmp(buf,"quit") == 0) break;
+    
+    if (strcmp(buf,"quit") == 0){
+        break;
+    }
+
     sscanf(buf,"%d",&n);
     root = insert(root,n);
   }
 
   printf("in-order traversal:\n");
   InOrder(root);  
+  
   printf("pre-order traversal:\n");
   PreOrder(root);  
+  
   printf("post-order traversal:\n");
   PostOrder(root);  
   return 0;
